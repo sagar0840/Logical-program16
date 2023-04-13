a63f816fb49fc92744158071ce6941d8b31c9207
sagar0840 <sagarkale55556@gmail.com>
2023-04-13 20:45:33 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        //Swap values of two integer variables without using third variable
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a * b;      
+            b = a / b;      
+            a = a / b;     
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+    }
+
